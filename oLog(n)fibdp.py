d0cf953a841db05a84adc7d837865923c7bd5071
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2022-12-19 17:05:26 -0500

    Update oLog(n)fibdp.py

--- oLog(n)fibdp.py
+++ oLog(n)fibdp.py
@@ -3,17 +3,17 @@ dp = [-1 for i in range(n+1)]
 
 def fib(n):
     if (n<=1):
-        return n;
+        return n
 
     if (dp[n - 1] != -1):
-        first = dp[n - 1];
+        first = dp[n - 1]
     else:
-        first = fib(n - 1);
+        first = fib(n - 1)
     if (dp[n - 2] != -1):
-        second = dp[n - 2];
+        second = dp[n - 2]
     else:
-        second = fib(n - 2);
-    dp[n] = first + second;
+        second = fib(n - 2)
+    dp[n] = first + second
 
-    return dp[n];
+    return dp[n]
 print(fib(n))
